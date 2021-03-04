8a03937aea8d858edbeb3922ccfb1d045f6c3718
spaceisfun <jakefriedman@live.com>
2021-03-03 20:20:48 -0800

    wip

---
+++ memes/CheckTwitter.ps1
@@ -0,0 +1,21 @@
+Write-Progress -Activity "Checking Twitter interactions...." -PercentComplete 0
+Start-Sleep .5
+Write-Progress -Activity "Checking Twitter interactions...." -PercentComplete 25
+Start-Sleep .5
+Write-Progress -Activity "Checking Twitter interactions...." -PercentComplete 50
+Start-Sleep .5
+Write-Progress -Activity "Checking Twitter interactions...." -PercentComplete 75
+Start-Sleep .5
+Write-Progress -Activity "Checking Twitter interactions...." -PercentComplete 100
+Start-Sleep 1
+Write-Progress -Activity "Checking Twitter interactions...." -Completed
+Write-Error
+"
+╔═══╗╔╗         ╔╗ ╔╗
+║╔═╗║║║        ╔╝╚╗║║
+║║ ║║║║ ╔══╗╔═╗╚╗╔╝║║
+║╚═╝║║║ ║╔╗║║╔╝ ║║ ╚╝
+║╔═╗║║╚╗║║═╣║║  ║╚╗╔╗
+╚╝ ╚╝╚═╝╚══╝╚╝  ╚═╝╚╝
+`nTwitter troll detected in your replies!
+"
---
+++ memes/CheckTwitterInteractions.ps1
@@ -0,0 +1,36 @@
+$c = new-factoryorchestratorclient 127.0.0.1
+Write-Host "Connecting to Factory Orchestrator service running on your Windows Subsystem for Linux instance..."
+$c.Connect()
+Start-Sleep 1
+Write-Host "Connected!"
+Write-Host ""
+$str = "Service OS version information: " + $c.GetOSVersionString()
+Write-Host $str
+Start-Sleep 1
+$a = Read-Host "Do you wish to check your Twitter interactions?"
+Write-Host "Running the CheckTwitterInteractions TaskList on $($c.GetOSVersionString()) Factory Orchestrator..."
+$tl = $c.QueryTaskList("9810e793-02b0-4eda-bc96-2ffbd343dab3")
+foreach ($t in $tl.Tasks)
+{
+    $r = $c.QueryTaskRun($t.LatestTaskRunGuid)
+    $last = 0
+    Write-Host "    Task $($t.Name) is running.... Real-time output:`n"
+    while (-not $r.TaskRunComplete)
+    {
+        while ($last -lt  $r.TaskOutput.Count)
+        {
+            Write-Host $r.TaskOutput[$last++]
+        }
+
+        $r = $c.QueryTaskRun($t.LatestTaskRunGuid)
+    }
+
+    while ($last -lt  $r.TaskOutput.Count)
+    {
+        Write-Host $r.TaskOutput[$last++]
+    }
+
+    Write-Host "`n    Task $($t.Name) is complete!"
+}
+
+Write-Host "Thanks for using Factory Orchestrator :)"
